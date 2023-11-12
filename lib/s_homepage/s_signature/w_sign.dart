4481c0167519425b70051251c82cf6a08b697406
Joshua Fletcher <jfletcher20@student.foi.hr>
2023-11-12 16:04:34 +0100

    Implemented signature creation and verification.

--- lib/s_homepage/s_signature/w_sign.dart
+++ lib/s_homepage/s_signature/w_sign.dart
@@ -1,4 +1,16 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
+import 'dart:io';
+
+import 'package:cryptography_flutter/file_management/u_file_creation.dart';
+import 'package:cryptography_flutter/file_management/u_keys.dart';
+import 'package:cryptography_flutter/w_file_contents.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pointycastle/digests/sha256.dart';
+import 'package:pointycastle/pointycastle.dart';
+import 'package:pointycastle/signers/rsa_signer.dart';
 
 class SignFileWidget extends StatefulWidget {
   const SignFileWidget({super.key});
@@ -8,8 +20,90 @@ class SignFileWidget extends StatefulWidget {
 }
 
 class _SignFileWidgetState extends State<SignFileWidget> {
+  File? selectedFile;
+  bool isDigestButtonEnabled = false;
+  String digestText = "";
+
   @override
   Widget build(BuildContext context) {
-    return Text("Sign a document.");
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ...wrap("Selected file: ", Text(selectedFile?.path ?? "None")),
+        ...wrap("File contents: ", FileContentsWidget(file: selectedFile)),
+        ElevatedButton(onPressed: _pickFile, child: const Text("Select File")),
+        const SizedBox(height: 16),
+        if (digestText.isNotEmpty) ..._digestTextWidget,
+        ElevatedButton(
+          onPressed: isDigestButtonEnabled ? _calculateDigestOfFile : null,
+          child: const Text("Calculate Digest"),
+        ),
+      ],
+    );
+  }
+
+  List<Widget> get _digestTextWidget {
+    TextStyle style = Theme.of(context).textTheme.displaySmall!.copyWith(color: Colors.green[200]);
+    return wrap(
+      "Digest text: ",
+      Text(digestText, style: style),
+    );
+  }
+
+  List<Widget> wrap(String label, Widget child) {
+    return [
+      Text(label),
+      Container(
+        padding: const EdgeInsets.all(20),
+        height: 100,
+        child: SingleChildScrollView(child: child),
+      ),
+    ];
+  }
+
+  Future<void> _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      setState(() {
+        selectedFile = File(result.files.single.path!);
+        isDigestButtonEnabled = true;
+      });
+    }
+  }
+
+  Future<void> _calculateDigestOfFile() async {
+    if (selectedFile == null) return;
+    final fileName = selectedFile!.path.split("\\").last.replaceAll(".txt", "");
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+    String asym = await calculateDigestOfFile();
+    try {
+      await FileManager.saveToFile(
+        "${fileName}_digest_$timestamp.txt",
+        asym,
+        createDir: true,
+        additionalPath: "signature/digest",
+      );
+    } catch (e) {
+      if (context.mounted)
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  Future<String> calculateDigestOfFile() async {
+    String fileContents = FileManager.readFromFile(selectedFile!);
+
+    AsymmetricKeyParameter<RSAPrivateKey> privateKey =
+        PrivateKeyParameter(await KeysManager.privateKey());
+
+    RSASigner signer = RSASigner(SHA256Digest(), "0609608648016503040201");
+    signer.init(true, privateKey);
+
+    RSASignature digest = signer.generateSignature(Uint8List.fromList(fileContents.codeUnits));
+
+    setState(() {
+      digestText = String.fromCharCodes(digest.bytes);
+    });
+
+    return String.fromCharCodes(digest.bytes);
   }
 }
--- lib/s_homepage/s_signature/w_verify.dart
+++ lib/s_homepage/s_signature/w_verify.dart
@@ -1,4 +1,16 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
+import 'dart:io';
+
+import 'package:cryptography_flutter/file_management/u_file_creation.dart';
+import 'package:cryptography_flutter/file_management/u_keys.dart';
+import 'package:cryptography_flutter/w_file_contents.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pointycastle/digests/sha256.dart';
+import 'package:pointycastle/pointycastle.dart';
+import 'package:pointycastle/signers/rsa_signer.dart';
 
 class VerifySignatureWidget extends StatefulWidget {
   const VerifySignatureWidget({super.key});
@@ -8,8 +20,98 @@ class VerifySignatureWidget extends StatefulWidget {
 }
 
 class _VerifySignatureWidgetState extends State<VerifySignatureWidget> {
+  File? originalFile, signedFile;
+  bool isVerificationButtonEnabled = false;
+  bool verificationResult = false;
+
   @override
   Widget build(BuildContext context) {
-    return Text("Verify a signature.");
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ...wrap("Original file: ", Text(originalFile?.path ?? "None")),
+        ...wrap("File contents: ", FileContentsWidget(file: originalFile)),
+        ElevatedButton(onPressed: _pickOriginalFile, child: const Text("Select Original File")),
+        const SizedBox(height: 16),
+        ...wrap("Signed file: ", Text(signedFile?.path ?? "None")),
+        ...wrap("File contents: ", FileContentsWidget(file: signedFile)),
+        ElevatedButton(onPressed: _pickSignedFile, child: const Text("Select Signed File")),
+        const SizedBox(height: 16),
+        ..._verificationResultWidget,
+        ElevatedButton(
+          onPressed: isVerificationButtonEnabled ? _verifyFile : null,
+          child: const Text("Verify Signature"),
+        ),
+      ],
+    );
+  }
+
+  List<Widget> get _verificationResultWidget {
+    TextStyle style = Theme.of(context)
+        .textTheme
+        .displaySmall!
+        .copyWith(color: verificationResult ? Colors.green[200] : Colors.red[200]);
+    return wrap(
+      "Verification result: ",
+      Text(verificationResult ? "All is in order" : "Did not pass verification", style: style),
+    );
+  }
+
+  List<Widget> wrap(String label, Widget child) {
+    return [
+      Text(label),
+      Container(
+        padding: const EdgeInsets.all(20),
+        height: 100,
+        child: SingleChildScrollView(child: child),
+      ),
+    ];
+  }
+
+  Future<void> _pickOriginalFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      setState(() {
+        originalFile = File(result.files.single.path!);
+        isVerificationButtonEnabled = originalFile != null && signedFile != null;
+      });
+    }
+  }
+
+  Future<void> _pickSignedFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      setState(() {
+        signedFile = File(result.files.single.path!);
+        isVerificationButtonEnabled = originalFile != null && signedFile != null;
+      });
+    }
+  }
+
+  Future<void> _verifyFile() async {
+    if (signedFile == null || originalFile == null) return;
+    await calculateDigestOfFile();
+  }
+
+  Future<bool> calculateDigestOfFile() async {
+    String signedFileContents = FileManager.readFromFile(signedFile!);
+    String originalFileContents = FileManager.readFromFile(originalFile!);
+
+    AsymmetricKeyParameter<RSAPublicKey> publicKey =
+        PublicKeyParameter(await KeysManager.publicKey());
+
+    RSASigner signer = RSASigner(SHA256Digest(), "0609608648016503040201");
+    signer.init(false, publicKey);
+
+    bool verification = signer.verifySignature(
+      Uint8List.fromList(originalFileContents.codeUnits),
+      RSASignature(Uint8List.fromList(signedFileContents.codeUnits)),
+    );
+
+    setState(() {
+      verificationResult = verification;
+    });
+
+    return verification;
   }
 }
