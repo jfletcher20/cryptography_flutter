8c540cc8882809a0b2c9580e79c6239812dbc84a
Joshua Fletcher <jfletcher20@student.foi.hr>
2024-11-23 17:01:42 +0100

    Uploading Windows official release version.

--- lib/s_homepage/s_signature/s_signature.dart
+++ lib/s_homepage/s_signature/s_signature.dart
@@ -1,7 +1,6 @@
 import 'package:cryptography_flutter/s_homepage/s_signature/w_verify.dart';
-import 'package:cryptography_flutter/s_homepage/s_tab_screen.dart';
-import 'package:cryptography_flutter/s_homepage/w_not_yet_implemented.dart';
 import 'package:cryptography_flutter/s_homepage/s_signature/w_sign.dart';
+import 'package:cryptography_flutter/s_homepage/s_tab_screen.dart';
 import 'package:flutter/material.dart';
 
 class SignatureScreen extends StatelessWidget {
